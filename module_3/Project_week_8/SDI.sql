899167dfabc467af212e99b7291962d443498dcf
moldypeaches <adelejulietmartin@gmail.com>
2023-06-05 12:43:47 +0200

    project

--- module_3/Project_week_8/SDI.sql
+++ module_3/Project_week_8/SDI.sql
@@ -1,29 +1,41 @@
-use films_data;
+use film_data;
 
-#1. What are the most common movie genres?
-select count(f.film_id) as "FILM COUNT", 
-g.genre as "GENRE"
-from films f 
-left join film_genre fg 
-on fg.film_id = f.film_id
-inner join genres g
-on g.genre_id = fg.genre_id
-group by 2
-order by 1 desc
-limit 10;
+#1. What are the most common and popular movie genres?
 
-#2. #2. What are the most popular movie genres?
-SELECT avg(f.popularity) AS "AVG POPULARITY",
-       g.genre AS "GENRE"
-FROM films f
-INNER JOIN film_genre fg ON f.film_id = fg.film_id
-INNER JOIN genres g ON g.genre_id = fg.genre_id
-GROUP BY g.genre
+SELECT COUNT(f.film_id) AS "FILM COUNT WOMEN", 
+AVG(f.popularity),
+g.genre AS "GENRE"
+FROM films f 
+right join directors d 
+on f.director_id = d.director_id
+LEFT JOIN film_genre fg 
+ON fg.film_id = f.film_id
+INNER JOIN genres g
+ON g.genre_id = fg.genre_id
+where d.director_gender = 'female'
+GROUP BY 3
 ORDER BY 1 DESC
 LIMIT 10;
 
-#3 Who directed the most movies?
-select count(film_id) as "FILM COUNT", 
+SELECT COUNT(f.film_id) AS "FILM COUNT MEN", 
+AVG(f.popularity),
+g.genre AS "GENRE"
+FROM films f 
+right join directors d 
+on f.director_id = d.director_id
+LEFT JOIN film_genre fg 
+ON fg.film_id = f.film_id
+INNER JOIN genres g
+ON g.genre_id = fg.genre_id
+where d.director_gender = 'male'
+GROUP BY 3
+ORDER BY 1 DESC
+LIMIT 10;
+
+
+#2. Who directed the most movies?
+
+select count(f.film_id) as "FILM COUNT", 
 d.director as "DIRECTOR", 
 d.director_gender as "GENDER"
 from films f 
@@ -33,37 +45,45 @@ group by 2, 3
 order by 1 desc
 limit 10;
 
-#4 Where are most of the movies produced?
-select count(f.film_id) as "FILM COUNT",
-c.country as "COUNTRY"
-from films f 
-right join directors d 
-on f.director_id = d.director_id
-inner join film_country fc
-on fc.film_id = f.film_id
-inner join countries c
-on c.country_id = fc.country_id
-where director_gender = 'female'
-group by 2
-order by 1 desc
-limit 10;
 
-select count(f.film_id) as "FILM COUNT",
-c.country as "COUNTRY"
-from films f 
-right join directors d 
-on f.director_id = d.director_id
-inner join film_country fc
-on fc.film_id = f.film_id
-inner join countries c
-on c.country_id = fc.country_id
-where director_gender = 'male'
-group by 2
-order by 1 desc
-limit 10;
+#3 Who produced the most multi-cultural movies?
 
-#5. Question about popularity
-select avg(popularity)
+SELECT count(f.film_id) as "MALE FILMS",
+f.spoken_languages as "NUMBER OF SPOKEN LANGUAGES"
+FROM films f 
+RIGHT JOIN directors d 
+on f.director_id = d.director_id
+WHERE spoken_languages > 1 
+AND director_gender = 'male'
+group by 2;
+
+SELECT count(f.film_id) as "FEMALE FILMS",
+f.spoken_languages as "NUMBER OF SPOKEN LANGUAGES"
+FROM films f 
+RIGHT JOIN directors d 
+on f.director_id = d.director_id
+WHERE spoken_languages > 1 
+AND director_gender = 'female'
+group by 2;
+
+#4. Counting unique film directors
+
+SELECT count(distinct d.director) as "DISTINCT MALE DIRECTORS"
+from directors d 
+where d.director_gender = 'male'
+ORDER BY 1 DESC
+LIMIT 10;
+
+SELECT count(distinct d.director) as "DISTINCT FEMALE DIRECTORS"
+from directors d 
+where d.director_gender = 'female'
+ORDER BY 1 DESC
+LIMIT 10;
+
+
+#5. What is the average popularity of films?
+
+select avg(popularity) as "AVG POPULARITY"
 from films f 
 right join directors d 
 on f.director_id = d.director_id
@@ -71,7 +91,7 @@ left join film_genre fg
 on fg.film_id = f.film_id
 inner join genres g
 on g.genre_id = fg.genre_id
-where director_gender in ('female' , 'male')
-group by director_gender
+where d.director_gender in ('male', 'female')
+group by d.director_gender
 order by 1 desc
 limit 10;
